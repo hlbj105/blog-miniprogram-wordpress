9808caaa2bdb6d9bc4678e9d17308745c3ea4900
Jason <hlbj105@qq.com>
2020-07-02 20:21:30 +0800

    修改成自己的配置文件

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 // 请求默认地址
-export const REQUEST_BASE_URL = 'https://www.xuanmo.xin'
+export const REQUEST_BASE_URL = 'https://blog.huangliangbo.com'
 
 // 请求超时时间，单位：s
 export const REQUEST_TIMEOUT = 30
